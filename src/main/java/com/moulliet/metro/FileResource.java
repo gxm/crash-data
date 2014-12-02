7d32382dd74a432fc3012bc6e6e179c9dc160295
gxm <moulliet@gmail.com>
2014-12-01 22:01:55 -0800

    adding redirects

--- src/main/java/com/moulliet/metro/FileResource.java
+++ src/main/java/com/moulliet/metro/FileResource.java
@@ -5,16 +5,33 @@ import org.apache.commons.io.FileUtils;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 
-@Path("/file")
+@Path("/")
 public class FileResource {
 
     @GET
-    @Path("{path:.*}")
+    public Response rootRedirect() throws IOException {
+        return redirect();
+    }
+
+    @GET
+    @Path("/file")
+    public Response fileRedirect() throws IOException {
+        return redirect();
+    }
+
+    @GET
+    @Path("/file/{path:.*}")
     public String get(@PathParam("path") String path) throws IOException {
         File file = new File(Config.getConfig().getString("public.dir") + path);
         return FileUtils.readFileToString(file);
+    }
+
+    private Response redirect() {
+        return Response.temporaryRedirect(URI.create("/file/index.html")).build();
     }
 }
