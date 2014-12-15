0a150b265b95940a972efc0827a3ffd57b47bada
gxm <moulliet@gmail.com>
2014-12-14 22:03:02 -0800

    cleanup

--- src/main/java/com/moulliet/metro/Statics.java
+++ src/main/java/com/moulliet/metro/Statics.java
@@ -1,9 +1,9 @@
 package com.moulliet.metro;
 
-import com.moulliet.metro.dataset.DatasetService;
+import com.moulliet.metro.load.DataService;
 import com.moulliet.metro.mongo.MongoDao;
 
 public class Statics {
     public static final MongoDao mongoDao = new MongoDao("crashes");
-    public static final DatasetService datasetService = new DatasetService();
+    public static final DataService datasetService = new DataService();
 }
