a55aea9790047397a957a54eb54f07df3684d432
gxm <moulliet@gmail.com>
2014-12-15 12:19:46 -0800

    gretting upload refresh to work

--- public/js/datasets.js
+++ public/js/datasets.js
@@ -6,7 +6,6 @@ function DatasetsController($scope, $http, $location) {
 
     $scope.datasets = [];
     $scope.message = 'loading...';
-    $scope.progressVisible = false
 
     $scope.loadData = function loadData() {
         $http.get($scope.host + 'datasets')
@@ -36,44 +35,32 @@ function DatasetsController($scope, $http, $location) {
         $scope.$apply(function (scope) {
             console.log('files:', element.files);
             // Turn the FileList object into an Array
-            scope.files = []
+            scope.files = [];
             for (var i = 0; i < element.files.length; i++) {
                 scope.files.push(element.files[i])
             }
-            scope.progressVisible = false
         });
     };
 
     $scope.uploadFile = function () {
-        var fd = new FormData()
+        var fd = new FormData();
         for (var i in $scope.files) {
             fd.append("file", $scope.files[i])
         }
-        console.log('$scope.datasetName', $scope.datasetName);
         fd.append("datasetName", $scope.datasetName);
-        var xhr = new XMLHttpRequest()
-        xhr.upload.addEventListener("progress", uploadProgress, false)
-        xhr.addEventListener("load", uploadComplete, false)
-        xhr.addEventListener("error", uploadFailed, false)
-        xhr.addEventListener("abort", uploadCanceled, false)
-        xhr.open("POST", "/datasets")
-        $scope.progressVisible = true
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", uploadComplete, false);
+        xhr.addEventListener("error", uploadFailed, false);
+        xhr.addEventListener("abort", uploadCanceled, false);
+        xhr.open("POST", "/datasets");
         xhr.send(fd)
-    }
-
-    function uploadProgress(evt) {
-        $scope.$apply(function () {
-            if (evt.lengthComputable) {
-                $scope.progress = Math.round(evt.loaded * 100 / evt.total)
-            } else {
-                $scope.progress = 'unable to compute'
-            }
-        })
-    }
+    };
 
     function uploadComplete(evt) {
-        $scope.datasets = JSON.parse(evt.target.responseText);
-        $scope.message = 'upload complete ' + new Date();
+        $scope.$apply(function () {
+            $scope.datasets = JSON.parse(evt.target.responseText);
+            $scope.message = 'upload complete ' + new Date();
+        });
     }
 
     function uploadFailed(evt) {
@@ -81,9 +68,6 @@ function DatasetsController($scope, $http, $location) {
     }
 
     function uploadCanceled(evt) {
-        $scope.$apply(function () {
-            $scope.progressVisible = false
-        })
         alert("The upload has been canceled by the user or the browser dropped the connection.")
     }
 }
