13d5552cebc65848044a9bad25ac6b91540d8765
gxm <moulliet@gmail.com>
2014-12-27 08:26:14 -0800

    fixing int test

--- src/test/java/com/moulliet/metro/IntegrationTest.java
+++ src/test/java/com/moulliet/metro/IntegrationTest.java
@@ -24,15 +24,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Full end to end integration test of server, through the Http interface and using Mongo
  */
 public class IntegrationTest {
     private static final Logger logger = LoggerFactory.getLogger(IntegrationTest.class);
-    private final ObjectMapper mapper = new ObjectMapper();
-    private Client client = ClientCreator.cached();
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private static Client client = ClientCreator.cached();
     public static final String ROOT_URL = "http://localhost:7070/";
     public static final String DATASETS_URL = ROOT_URL + "datasets";
     public static final String CRASH_URL = ROOT_URL + "metro/47/44/-121/-124?callback=stuff";
@@ -41,6 +40,9 @@ public class IntegrationTest {
     public static void beforeClass() throws Exception {
         System.setProperty("config.properties", "/Users/greg/code/crash-data/config/test/crash-data.properties");
         CrashServiceMain.startResources(7070);
+        deleteExisting();
+        loadFile();
+        setActive();
     }
 
     @AfterClass
@@ -48,23 +50,9 @@ public class IntegrationTest {
         CrashServiceMain.stop();
     }
 
-    @Test
-    public void testUpload() throws IOException {
-        deleteExisting();
-        loadFile();
-        setActive();
 
-        ClientResponse response = client.resource(CRASH_URL).get(ClientResponse.class);
-        assertEquals(200, response.getStatus());
-        String entity = trimEntity(response.getEntity(String.class));
-        System.out.println(entity);
-        JsonNode rootNode = mapper.readTree(entity);
-        assertEquals(30, rootNode.get("max").asInt());
-        assertEquals(953, rootNode.get("total").asInt());
-        assertTrue(entity.contains("{\"lat\":\"45.5591\",\"lng\":\"-122.6615\",\"count\":30,\"radius\":24}"));
-    }
 
-    private void setActive() throws IOException {
+    private static void setActive() throws IOException {
         ObjectNode dataset = (ObjectNode) getDatasets().get(0);
         dataset.put("active", true);
         logger.info("dataset active {}", dataset);
@@ -77,17 +65,17 @@ public class IntegrationTest {
         assertEquals(200, put.getStatus());
     }
 
-    private void deleteExisting() throws IOException {
+    private static void deleteExisting() throws IOException {
         for (JsonNode node : getDatasets()) {
             client.resource(DATASETS_URL + "/" + node.get("name").asText()).delete();
         }
     }
 
-    private JsonNode getDatasets() throws IOException {
+    private static JsonNode getDatasets() throws IOException {
         return mapper.readTree(client.resource(DATASETS_URL).get(String.class));
     }
 
-    private String loadFile() throws IOException {
+    private static String loadFile() throws IOException {
         String name = "test-" + RandomStringUtils.randomAlphabetic(6);
         FormDataMultiPart multiPart = new FormDataMultiPart();
         FormDataContentDisposition contentDisposition = FormDataContentDisposition
@@ -105,189 +93,143 @@ public class IntegrationTest {
         return name;
     }
 
-    /*
-
-    @Test
-    public void testAlcohol() throws IOException {
-        ClientResponse response = client.resource(URL + "&alcohol=true").get(ClientResponse.class);
+    private JsonNode getJsonNode(String params) throws IOException {
+        ClientResponse response = client.resource(CRASH_URL + params).get(ClientResponse.class);
         assertEquals(200, response.getStatus());
         String entity = trimEntity(response.getEntity(String.class));
         System.out.println(trimEntity(entity));
-        JsonNode rootNode = mapper.readTree(entity);
-        assertEquals(1, rootNode.get("max").asInt());
-        assertEquals(1, rootNode.get("total").asInt());
-        assertEquals(1, rootNode.get("summary").get("alcohol").asInt());
+        return mapper.readTree(entity);
+    }
+
+    @Test
+    public void testAll() throws IOException {
+        JsonNode rootNode = getJsonNode("");
+        assertEquals(30, rootNode.get("max").asInt());
+        assertEquals(953, rootNode.get("total").asInt());
+        //assertTrue(entity.contains("{\"lat\":\"45.5591\",\"lng\":\"-122.6615\",\"count\":30,\"radius\":24}"));
+    }
+
+    @Test
+    public void testAlcohol() throws IOException {
+        JsonNode rootNode = getJsonNode("&alcohol=true");
+        assertEquals(2, rootNode.get("max").asInt());
+        assertEquals(52, rootNode.get("total").asInt());
+        assertEquals(52, rootNode.get("summary").get("alcohol").asInt());
+        //assertTrue(entity.contains("{\"lat\":\"45.5591\",\"lng\":\"-122.6615\",\"count\":30,\"radius\":24}"));
     }
 
     @Test
     public void testPeds() throws IOException {
-        ClientResponse response = client.resource(URL + "&peds=true&cars=false&bikes=false").get(ClientResponse.class);
-        assertEquals(200, response.getStatus());
-        String entity = trimEntity(response.getEntity(String.class));
-        System.out.println(trimEntity(entity));
-        JsonNode rootNode = mapper.readTree(entity);
-        assertEquals(1, rootNode.get("max").asInt());
-        assertEquals(1, rootNode.get("total").asInt());
-        assertEquals(1, rootNode.get("summary").get("peds").asInt());
+        JsonNode rootNode = getJsonNode("&peds=true&cars=false&bikes=false");
+        assertEquals(3, rootNode.get("max").asInt());
+        assertEquals(31, rootNode.get("total").asInt());
+        assertEquals(31, rootNode.get("summary").get("peds").asInt());
         assertEquals(0, rootNode.get("summary").get("bikes").asInt());
     }
 
     @Test
     public void testBikes() throws IOException {
-        ClientResponse response = client.resource(URL + "&peds=false&cars=false&bikes=true").get(ClientResponse.class);
-        assertEquals(200, response.getStatus());
-        String entity = trimEntity(response.getEntity(String.class));
-        System.out.println(trimEntity(entity));
-        JsonNode rootNode = mapper.readTree(entity);
-        assertEquals(1, rootNode.get("max").asInt());
-        assertEquals(1, rootNode.get("total").asInt());
+        JsonNode rootNode = getJsonNode("&peds=false&cars=false&bikes=true");
+        assertEquals(2, rootNode.get("max").asInt());
+        assertEquals(40, rootNode.get("total").asInt());
         assertEquals(0, rootNode.get("summary").get("peds").asInt());
-        assertEquals(1, rootNode.get("summary").get("bikes").asInt());
+        assertEquals(40, rootNode.get("summary").get("bikes").asInt());
+        assertEquals(0, rootNode.get("summary").get("cars").asInt());
     }
 
     @Test
     public void testPedsAndBikes() throws IOException {
-        ClientResponse response = client.resource(URL + "&peds=true&cars=false&bikes=true").get(ClientResponse.class);
-        assertEquals(200, response.getStatus());
-        String entity = trimEntity(response.getEntity(String.class));
-        System.out.println(trimEntity(entity));
-        JsonNode rootNode = mapper.readTree(entity);
-        assertEquals(2, rootNode.get("max").asInt());
-        assertEquals(2, rootNode.get("total").asInt());
-        assertEquals(1, rootNode.get("summary").get("peds").asInt());
-        assertEquals(1, rootNode.get("summary").get("bikes").asInt());
+        JsonNode rootNode = getJsonNode("&peds=true&cars=false&bikes=true");
+        assertEquals(5, rootNode.get("max").asInt());
+        assertEquals(71, rootNode.get("total").asInt());
+        assertEquals(31, rootNode.get("summary").get("peds").asInt());
+        assertEquals(40, rootNode.get("summary").get("bikes").asInt());
     }
 
     @Test
     public void testWet() throws IOException {
-        ClientResponse response = client.resource(URL + "&wet=true&dry=false&snowIce=false").get(ClientResponse.class);
-        assertEquals(200, response.getStatus());
-        String entity = trimEntity(response.getEntity(String.class));
-        System.out.println(trimEntity(entity));
-        JsonNode rootNode = mapper.readTree(entity);
-        assertEquals(2, rootNode.get("max").asInt());
-        assertEquals(2, rootNode.get("total").asInt());
-        assertEquals(2, rootNode.get("summary").get("wet").asInt());
+        JsonNode rootNode = getJsonNode("&wet=true&dry=false&snowIce=false");
+        assertEquals(6, rootNode.get("max").asInt());
+        assertEquals(213, rootNode.get("total").asInt());
+        assertEquals(213, rootNode.get("summary").get("wet").asInt());
         assertEquals(0, rootNode.get("summary").get("snowIce").asInt());
-        assertEquals(0, rootNode.get("summary").get("bikes").asInt());
+        assertEquals(0, rootNode.get("summary").get("dry").asInt());
     }
 
     @Test
     public void testIce() throws IOException {
-        ClientResponse response = client.resource(URL + "&wet=false&dry=false&snowIce=true").get(ClientResponse.class);
-        assertEquals(200, response.getStatus());
-        String entity = trimEntity(response.getEntity(String.class));
-        System.out.println(trimEntity(entity));
-        JsonNode rootNode = mapper.readTree(entity);
+        JsonNode rootNode = getJsonNode("&wet=false&dry=false&snowIce=true");
         assertEquals(1, rootNode.get("max").asInt());
-        assertEquals(1, rootNode.get("total").asInt());
+        assertEquals(8, rootNode.get("total").asInt());
         assertEquals(0, rootNode.get("summary").get("wet").asInt());
-        assertEquals(1, rootNode.get("summary").get("snowIce").asInt());
-        assertEquals(0, rootNode.get("summary").get("bikes").asInt());
+        assertEquals(0, rootNode.get("summary").get("dry").asInt());
+        assertEquals(8, rootNode.get("summary").get("snowIce").asInt());
     }
 
     @Test
     public void testNight() throws IOException {
-        ClientResponse response = client.resource(URL + "&day=false&twilight=false&night=true").get(ClientResponse.class);
-        assertEquals(200, response.getStatus());
-        String entity = trimEntity(response.getEntity(String.class));
-        System.out.println(trimEntity(entity));
-        JsonNode rootNode = mapper.readTree(entity);
-        assertEquals(2, rootNode.get("max").asInt());
-        assertEquals(2, rootNode.get("total").asInt());
-        assertEquals(2, rootNode.get("summary").get("night").asInt());
+        JsonNode rootNode = getJsonNode("&day=false&twilight=false&night=true");
+        assertEquals(11, rootNode.get("max").asInt());
+        assertEquals(218, rootNode.get("total").asInt());
+        assertEquals(218, rootNode.get("summary").get("night").asInt());
         assertEquals(0, rootNode.get("summary").get("day").asInt());
         assertEquals(0, rootNode.get("summary").get("twilight").asInt());
     }
 
     @Test
     public void testTwilightNight() throws IOException {
-        ClientResponse response = client.resource(URL + "&day=false&twilight=true&night=true").get(ClientResponse.class);
-        assertEquals(200, response.getStatus());
-        String entity = trimEntity(response.getEntity(String.class));
-        System.out.println(trimEntity(entity));
-        JsonNode rootNode = mapper.readTree(entity);
-        assertEquals(3, rootNode.get("max").asInt());
-        assertEquals(3, rootNode.get("total").asInt());
-        assertEquals(2, rootNode.get("summary").get("night").asInt());
+        JsonNode rootNode = getJsonNode("&day=false&twilight=true&night=true");
+        assertEquals(12, rootNode.get("max").asInt());
+        assertEquals(288, rootNode.get("total").asInt());
+        assertEquals(218, rootNode.get("summary").get("night").asInt());
         assertEquals(0, rootNode.get("summary").get("day").asInt());
-        assertEquals(1, rootNode.get("summary").get("twilight").asInt());
+        assertEquals(70, rootNode.get("summary").get("twilight").asInt());
     }
 
     @Test
     public void testYear() throws IOException {
-        ClientResponse response = client.resource(URL +
-                "&y2007=false&y2008=true&y2009=false&y2010=false&y2011=false&y2012=false&y2013=true").get(ClientResponse.class);
-        assertEquals(200, response.getStatus());
-        String entity = trimEntity(response.getEntity(String.class));
-        System.out.println(trimEntity(entity));
-        JsonNode rootNode = mapper.readTree(entity);
-        assertEquals(2, rootNode.get("max").asInt());
-        assertEquals(2, rootNode.get("total").asInt());
+        JsonNode rootNode = getJsonNode("&y2007=false&y2008=true&y2009=false&y2010=true&y2011=false&y2012=false&y2013=false");
+        assertEquals(12, rootNode.get("max").asInt());
+        assertEquals(208, rootNode.get("total").asInt());
     }
 
     @Test
-    public void testTypeAll() throws IOException {
-        ClientResponse response = client.resource(URL +
-                "&angle=true&headOn=false&rearEnd=true&sideSwipe=false&turning=true&other=true").get(ClientResponse.class);
-        assertEquals(200, response.getStatus());
-        String entity = trimEntity(response.getEntity(String.class));
-        System.out.println(trimEntity(entity));
-        JsonNode rootNode = mapper.readTree(entity);
-        assertEquals(5, rootNode.get("total").asInt());
+    public void testTypeMost() throws IOException {
+        JsonNode rootNode = getJsonNode("&angle=true&headOn=false&rearEnd=true&sideSwipe=false&turning=true&other=true");
+        assertEquals(853, rootNode.get("total").asInt());
     }
 
     @Test
     public void testTypeSome() throws IOException {
-        ClientResponse response = client.resource(URL +
-                "&angle=false&headOn=false&rearEnd=true&sideSwipe=false&turning=true&other=false").get(ClientResponse.class);
-        assertEquals(200, response.getStatus());
-        String entity = trimEntity(response.getEntity(String.class));
-        System.out.println(trimEntity(entity));
-        JsonNode rootNode = mapper.readTree(entity);
-        assertEquals(2, rootNode.get("total").asInt());
+        JsonNode rootNode = getJsonNode("&angle=false&headOn=false&rearEnd=true&sideSwipe=false&turning=true&other=false");
+        assertEquals(532, rootNode.get("total").asInt());
     }
 
     @Test
     public void testSeverity() throws IOException {
-        ClientResponse response = client.resource(URL +
-                "&fatal=true&injuryA=false&injuryB=true&injuryC=false&property=true").get(ClientResponse.class);
-        assertEquals(200, response.getStatus());
-        String entity = trimEntity(response.getEntity(String.class));
-        System.out.println(trimEntity(entity));
-        JsonNode rootNode = mapper.readTree(entity);
-        assertEquals(3, rootNode.get("total").asInt());
+        JsonNode rootNode = getJsonNode("&fatal=true&injuryA=false&injuryB=true&injuryC=false&property=true");
+        assertEquals(583, rootNode.get("total").asInt());
         assertEquals(1, rootNode.get("summary").get("fatal").asInt());
         assertEquals(0, rootNode.get("summary").get("injuryA").asInt());
-        assertEquals(1, rootNode.get("summary").get("injuryB").asInt());
+        assertEquals(118, rootNode.get("summary").get("injuryB").asInt());
         assertEquals(0, rootNode.get("summary").get("injuryC").asInt());
-        assertEquals(1, rootNode.get("summary").get("property").asInt());
+        assertEquals(464, rootNode.get("summary").get("property").asInt());
     }
 
     @Test
     public void testSeveritySome() throws IOException {
-        ClientResponse response = client.resource(URL +
-                "&fatal=false&injuryA=true&injuryB=false&injuryC=true&property=false").get(ClientResponse.class);
-        assertEquals(200, response.getStatus());
-        String entity = trimEntity(response.getEntity(String.class));
-        System.out.println(trimEntity(entity));
-        JsonNode rootNode = mapper.readTree(entity);
-        assertEquals(2, rootNode.get("total").asInt());
+        JsonNode rootNode = getJsonNode("&fatal=false&injuryA=true&injuryB=false&injuryC=true&property=false");
+        assertEquals(370, rootNode.get("total").asInt());
         assertEquals(0, rootNode.get("summary").get("fatal").asInt());
-        assertEquals(1, rootNode.get("summary").get("injuryA").asInt());
+        assertEquals(15, rootNode.get("summary").get("injuryA").asInt());
         assertEquals(0, rootNode.get("summary").get("injuryB").asInt());
-        assertEquals(1, rootNode.get("summary").get("injuryC").asInt());
+        assertEquals(355, rootNode.get("summary").get("injuryC").asInt());
         assertEquals(0, rootNode.get("summary").get("property").asInt());
     }
 
 
     private String trimEntity(String entity) {
         return StringUtils.removeEnd(StringUtils.removeStart(entity, "stuff("), ")");
-    }*/
-
-    private String trimEntity(String entity) {
-        return StringUtils.removeEnd(StringUtils.removeStart(entity, "stuff("), ")");
     }
-
 
 }
