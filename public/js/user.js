a9731d86e40eb6db53a690c4d81e36e346f9fa57
gxm <moulliet@gmail.com>
2014-12-28 17:01:10 -0800

    working on user login

--- public/js/user.js
+++ public/js/user.js
@@ -1,21 +1,53 @@
-function UserController($scope, $http, $location) {
+function UserController($scope, $http, $location, $cookieStore) {
 
     'use strict';
 
     new Common($scope, $http, $location);
 
-    $scope.data = [{name:'Crash Map', href:'crashes.html'}];
+    $scope.links = [
+        {name:'Crash Map', href:'crashes.html'},
+        {name:'Login', href:'login.html'}
+    ];
 
-    $scope.loadData = function loadData() {
-        $http.jsonp($scope.host + 'links?callback=JSON_CALLBACK')
+    //todo - gfm - 12/28/14 - get this to return user type
+
+    $scope.user = '';
+    $scope.password = '';
+    $scope.message = '';
+
+    $scope.loadLinks = function loadData() {
+        var user = $cookieStore.get('user');
+        console.log('user', user);
+        if (user) {
+            if (user.type === 'admin') {
+                $scope.links = [
+                    {name:'Crash Map', href:'crashes.html'},
+                    {name:'Data Sets', href:'datasets.html'},
+                    {name:'Logout', href:'logout.html'}
+                ];
+            }
+        }
+    };
+
+    $scope.loadLinks();
+
+    $scope.login = function login() {
+        console.log('login');
+        $http.post($scope.host + 'user',
+            { user: $scope.user, password: $scope.password })
             .success(function (data, status, headers) {
-                console.log('links', data);
-                $scope.data = data;
+                console.log('login!', data);
+                $cookieStore.put('user', data);
+                window.location.replace($scope.host + 'file/index.html');
             }).error(function (data, status, headers) {
-                console.log('error', status);
+                console.log('login error', status);
+                $scope.message = data || 'Unable to login ' + status;
             });
     };
 
-    $scope.loadData();
+    $scope.logout = function login() {
+        $cookieStore.put('user', {});
+        window.location.replace($scope.host + 'file/index.html');
+    }
 
 }
--- src/main/java/com/moulliet/metro/UserResource.java
+++ src/main/java/com/moulliet/metro/UserResource.java
@@ -1,6 +1,8 @@
 package com.moulliet.metro;
 
+import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ObjectNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,9 +23,18 @@ public class UserResource {
     }
 
     @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response login(@FormParam("user") String user, @FormParam("password") String password) {
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response login(String json) throws Exception {
+        JsonNode node = mapper.readTree(json);
+        String user = node.get("user").asText();
+        String password = node.get("password").asText();
         logger.info("got post! {} {}", user, password);
-        return Response.ok().build();
+        ObjectNode root = mapper.createObjectNode();
+        root.put("type", user);
+        if ("admin".equals(user)) {
+            root.put("key", "ABC");
+        }
+        return Response.ok(root.toString()).build();
     }
 }
